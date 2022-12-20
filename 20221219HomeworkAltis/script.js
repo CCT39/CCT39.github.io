5dd7e3f21ce2adf1281cb46aa41199530a29fee3
CCT39 <kyoto31205@gmail.com>
2022-12-20 15:52:45 +0800

    1219HW1 fixed

--- 20221219HomeworkAltis/script.js
+++ 20221219HomeworkAltis/script.js
@@ -1,41 +1,68 @@
-let carImages = [];
-let mainImg, prevBtn, postBtn, btns;
+let carImagesViaInternet = [], carImagesViaLocal = [];
+let mainImg, prevBtn, postBtn, btns, viaLocal, viaInternet, interval;
+let isViaInternet = false;
 let presentImgIndex = 0;
 
 window.onload = function (){
+    getElements();
+    generateCarImages();
+    setEventListeners();
+    mainImg.src = carImagesViaLocal[presentImgIndex];
+}
+
+function getElements(){
     mainImg = document.getElementById('main-img');
     prevBtn = document.querySelector('.prev');
     postBtn = document.querySelector('.post');
     btns = document.querySelectorAll('.btn');
-    generateCarImages();
-    mainImg.src = carImages[presentImgIndex];
-    
-    prevBtn.addEventListener('click', function(){changeImgs(-1);});
-    postBtn.addEventListener('click', function(){changeImgs(1);});
+    viaLocal = document.getElementById('local');
+    viaInternet = document.getElementById('internet');
 }
-
 
 function generateCarImages(){
     for(let i = 1; i < 61; i++){
-        carImages.push(`https://hotaicdn.azureedge.net/toyotaweb/360EXT_1_18_${i}.png`)
+        carImagesViaInternet.push(`https://hotaicdn.azureedge.net/toyotaweb/360EXT_1_18_${i}.png`);
+        carImagesViaLocal.push(`./AltisImg/AltisRed/360EXT_1_22_${i}.png`);
     }
+}
 
-    console.log(carImages);
+function setEventListeners(){
+    prevBtn.addEventListener('click', function(){changeImgs(-1);});
+    postBtn.addEventListener('click', function(){changeImgs(1);});
+    prevBtn.addEventListener('mousedown', function(){interval = window.setInterval(changeImgs, 100, -1);})
+    postBtn.addEventListener('mousedown', function(){interval = window.setInterval(changeImgs, 100, 1);})
+    prevBtn.addEventListener('mouseup', function(){clearInterval(interval);})
+    postBtn.addEventListener('mouseup', function(){clearInterval(interval);})
+    viaLocal.addEventListener('change', function(){
+        isViaInternet = false;
+        changeColour();
+    })
+    viaInternet.addEventListener('change', function(){
+        isViaInternet = true;
+        changeColour();
+    })
+}
+
+function changeColour(){
+    if (isViaInternet){
+        mainImg.src = carImagesViaInternet[presentImgIndex];
+    } else {
+        mainImg.src = carImagesViaLocal[presentImgIndex];
+    }
 }
 
 function changeImgs(num){
+    let imgArray = carImagesViaLocal;
+    if (isViaInternet){
+        imgArray = carImagesViaInternet;
+    }
     
-    let int = num;
-    if(isNaN(num)){
-        int = parseInt(num, 10);
-    }
-
+    let int = parseInt(num, 10);
     presentImgIndex += int;
-    while (presentImgIndex < 0) {
-        presentImgIndex += carImages.length;
-    }
-    if (presentImgIndex >= carImages.length){
-        presentImgIndex %= carImages.length
+    if (presentImgIndex < 0) {
+        presentImgIndex += imgArray.length;
+    } else if (presentImgIndex >= imgArray.length){
+        presentImgIndex -= imgArray.length;
     } 
-    mainImg.src = carImages[presentImgIndex];
+    mainImg.src = imgArray[presentImgIndex];
 }
